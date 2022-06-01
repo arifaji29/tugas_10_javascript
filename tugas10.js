f567f51109a901fc3580f30a4eaa8baf757068b4
Arif <arifajif@gmail.com>
2022-06-01 22:26:19 +0700

    tugas 10

---
+++ tugas10.js
@@ -0,0 +1,21 @@
+var x =1;
+console.log(x)
+x++;
+console.log(x+1)
+x++;
+console.log(x+3)
+x++;
+console.log(x+6)
+x++;
+console.log(x+10)
+x++;
+console.log(x+15)
+x++;
+console.log(x+21)
+x++;
+console.log(x+28)
+x++;
+console.log(x+36)
+x++;
+console.log(x+45)
+
